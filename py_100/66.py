a3cf51f2eed8ab58d008df0f4dc8e2d00fe2a9db
kimsr96 <kimsr_96@naver.com>
2021-01-12 17:58:55 +0900

    py_100_66

--- py_100/66.py
+++ py_100/66.py
@@ -1,11 +1,15 @@
 top = list(input().split())
 rule = list(input())
-
-for i in range(len(top)):
-    # print(top[i],end='')
+for i in range(len(top)): #입력을 각 단어로 나누기
+    b = []
     a = list(top[i])
-    for idx,char in enumerate(rule):
-        print(zip(int(idx),char))
-        
-
-    print(rule)
+    cnt=0
+    for j in range(len(top[i])): # 각 단어를 한 글자씩 나누기
+        for idx,char in enumerate(rule): #rule을 인덱스로 바꾸기
+            if a[j] == char:
+                cnt +=1
+                b.append(idx)
+    if sorted(b) == b:
+        print("가능")
+    else: 
+        print("불가능")
---
+++ py_100/67.py
@@ -0,0 +1,4 @@
+if "a" < "b" :
+    print("hi")
+else: 
+    print("no")
